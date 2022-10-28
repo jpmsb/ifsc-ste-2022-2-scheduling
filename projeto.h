b4df80dbca78d6688e8f92d69a75db4c9601823d
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2022-10-28 00:09:00 -0300

    Ajustando o programa para realizar as tarefas pedidas na atividade

--- projeto.h
+++ projeto.h
@@ -4,10 +4,10 @@
 #include "UART.h"
 #include "ADC.h"
 #include "GPIO_Pin.h"
+#include <stdio.h>
 
 static void busy_wait_delay(int times){
-    while (times--)
-    {
+    while (times--){
         unsigned long x = 0x7ffff;
         while(x--) __asm__ ("nop");
     }
